6875becdbe8a34a13cde57bdafbb463c20311e74
javi1997-laravel <56174714+javi1997-laravel@users.noreply.github.com>
2020-05-05 16:47:57 -0400

    actualizar index

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+﻿<?php 
 	include 'php/conexion.php';
 	session_start();
 
@@ -12,16 +12,22 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="viewport" content="width=device‐width, initial‐scale=1.0,
+maximum‐scale=1.0, user‐scalable=no">
 	<title>Iniciar sesión</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> 
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-	<main class="container">
-		<img src="img/logo1.png" class="corpo">
+
+	<center>
+	<main  class="container">
+		<img src="img/logo1.png" class="corpo" alt="">
 		<br>
+		<br>
+		<br>
+		<br>
+
 		<?php if ($mensaje == 1): ?>
 			<div class="alert alert-warning alert-dismissible fade show" role="alert" id="alert">
 				Usuario y contraseña incorrecta inténtalo nuevamente.
@@ -32,22 +38,18 @@
 			</div>
 		<?php endif ?>
 
-
-			<div class="login">
-			<h3 class="text-center">SICEP</h3>
-			<h5 class="text-center">Iniciar Sesión</h5>
-			<form method="post" action="php/entrar.php">
+		<div   class="login-box">	
+			<h3>SICEP</h3>
+			<h5>Iniciar Sesión</h5>
+			<form   method="post" action="php/entrar.php">
 				<div class="form-group">
-					<div class=" ">
-
 				<label for="usuario">Nombre de Usuario</label>
-					<input type="text" name="usuario" placeholder="Ingrese el nombre usuario" class="form-control">
+					<input type="text" name="usuario" placeholder="Ingrese su usuario" class="form-control">
 				</div>
 				<div class="form-group">
 				<label for="password">Contraseña</label>
 					<input type="password" name="password" placeholder="Ingrese la contraseña" class="form-control">
 				</div>
-				<div class="form-group col-lg-5"></div>
 				<div class="form-group">
 					<input type="submit" name="" class="btn btn-primary" value="Iniciar Sesión">
 				</div>
@@ -57,9 +59,9 @@
 
 	
 
+	</center>
 	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
-</body>
 </html>
 
